dbe3fe77915cfa024d6fbd77d5634609382711e1
William Sanville <wsanville@gmail.com>
2012-08-10 13:06:12 -0400

    Initial load.

---
+++ src/co/touchlab/hellowebview/HelloActivity.java
@@ -0,0 +1,43 @@
+package co.touchlab.hellowebview;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.webkit.WebView;
+
+public class HelloActivity extends Activity
+{
+    private WebView webView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        webView.setBackgroundColor(Color.WHITE);
+
+        // The URL below has the following meta tag, which disables WebView scaling:
+        // <meta name="viewport" content="target-densitydpi=device-dpi" />
+        webView.loadUrl("http://codinghero.ws/public/webview.html");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if (webView != null)
+        {
+            ViewParent parent = webView.getParent();
+            if (parent != null)
+            {
+                ViewGroup parentView = (ViewGroup)parent;
+                parentView.removeView(webView);
+            }
+        }
+    }
+}
